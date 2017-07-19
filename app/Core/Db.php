648050b58254453480fb4d307abd0865144d00f3
rehhg <rehhg@mail.ua>
2017-07-19 17:43:07 +0300

    connected to database

---
+++ app/Core/Db.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Core;
+
+class Db extends \PDO {
+    
+    private static $instance;
+    
+    private static $pathToConfigYml = 'app/Config/config.yml';
+    
+    /*
+     * Returns DB instance or create initial connection
+     * 
+     * @param
+     * @return $instance;
+     */
+    public static function getInstance() {
+        
+        $data = self::getDataFromYaml(self::$pathToConfigYml);
+           
+        if (!self::$instance) {
+            self::$instance = new \PDO($data["db_host"], $data["db_user"], $data["db_pass"]);
+            self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }
+       
+        return self::$instance;
+   
+    }
+    
+    /*
+     * Returns data from .yml files
+     * 
+     * @return $parsed;
+     */
+    public static function getDataFromYaml($path) {
+           
+        $yaml = \App\Core\App::getRootPath() . '/' . $path;
+        $parsed = yaml_parse_file($yaml);
+        
+        return $parsed;
+        
+    }
+    
+}
