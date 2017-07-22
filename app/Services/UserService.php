83e9a6ffa69366c8e2e8fd39707c796151f18392
rehhg <rehhg@mail.ua>
2017-07-22 19:25:25 +0300

    fixed bugs in UserServieces methods

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,8 +4,8 @@ namespace App\Services;
 
 class UserService extends Service {
     
-    public function createUser(User $user = null) {
-        $query = $this->db->prepare("INSER INTO users VALUES (NULL, ?, ?, NOW())");
+    public function createUser(\User $user = null) {
+        $query = $this->db->prepare("INSERT INTO users VALUES (NULL, ?, ?, NOW())");
         $query->bindParam(1, $user->username, \PDO::PARAM_STR);
         $query->bindParam(2, $user->email, \PDO::PARAM_STR);
         if(!$query->execute()) {
@@ -15,13 +15,13 @@ class UserService extends Service {
         return $user;
     }
     
-    public function getUser($id, User $user = null) {
+    public function getUser($id, \User $user = null) {
         $user = $this->db->query("SELECT * FROM users WHERE id = $id")->fetch(\PDO::FETCH_OBJ);
         
         return $user;
     }
     
-    public function updateUser($id, User $user = null) {
+    public function updateUser($id, \User $user = null) {
         $query = $this->db->prepare("UPDATE users SET name = ?, email = ? WHERE id = $id");
         $query->bindParam(1, $user->username, \PDO::PARAM_STR);
         $query->bindParam(2, $user->email, \PDO::PARAM_STR);
