e8c5411660d26946ec5e1509b379b406b6cf207b
rehhg <rehhg@mail.ua>
2017-08-11 18:37:01 +0300

    add UserControllerTest

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,15 +3,23 @@
 namespace App\Controllers;
 
 use App\Models\Model;
+use App\Services\Service;
 
 class BaseController {
-    
+
+    public $service;
+
+    public function setService(Service $service) {
+        $this->service = $service;
+    }
+
     protected function validate(Model $model) {
         $modelName = get_class($model);
         $modelClassName = substr($modelName, strrpos($modelName, "\\") + 1);
         $validatorName = "App\\Validators\\" . $modelClassName . 'Validator';
         $validator = new $validatorName;
-        
+
         return $validator->validate($model);
     }
+
 }
--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -2,20 +2,158 @@
 
 use PHPUnit\Framework\TestCase;
 
+use App\Models\User;
+
 class UserControllerTest extends TestCase {
+    
+    /**
+     * @dataProvider providerListAction
+     */
+    public function testForListAction($input) {
+        $service = $this->getMockBuilder("App\Services\UserService")
+                ->setConstructorArgs(['ut'])
+                ->getMock();
+        $userController = new App\Controllers\UserController();
+        $userController->setService($service);
+        
+        $service->expects($this->once())     
+                ->method("getAllUsers")
+                ->will($this->returnValue(new User()));
+        
+        $userController->listAction($service->getAllUsers($input));
+        
+    }
+    
+    public function providerListAction() {
+        return [
+            // input      
+            [null],
+            [new User()],
+            [1],
+            ['deleteAction']
+        ];
+    }
+    
+    /**
+     * @dataProvider providerCreateAction
+     */
+    public function testForCreateAction($exception, $input) {
+        $service = $this->getMockBuilder("App\Services\UserService")
+                ->setConstructorArgs(['ut'])
+                ->getMock();
+        $userController = new App\Controllers\UserController();
+        $userController->setService($service);
+        
+        if(!$exception){
+            $service->expects($this->once())     
+                ->method("createUser")
+                ->will($this->returnValue(new User()));
+            
+            $userController->createAction($service->createUser($input));
+        } else {
+            $this->expectException("TypeError");
+            $service->expects($this->any())     
+                ->method("createUser")
+                ->will($this->returnValue(new User()));
+            
+            $userController->createAction($service->createUser($input));
+        }
+    }
+    
+    public function providerCreateAction() {
+        return [
+            // exception    input      
+            [false,         null],
+            [false,         new User()],
+            [true,          1],
+            [true,          'sdfdg']
+        ];
+    }
+    
+    /**
+     * @dataProvider providerUpdateAction
+     */
+    public function testForUpdateAction($exception, $input) {
+        $service = $this->getMockBuilder("App\Services\UserService")
+                ->setConstructorArgs(['ut'])
+                ->getMock();
+        $userController = new App\Controllers\UserController();
+        $userController->setService($service);
+        
+        if(!$exception){
+            $service->expects($this->once())     
+                    ->method("updateUser")
+                    ->will($this->returnCallback('getArgument'));
+
+            $userController->updateAction($service->updateUser($input));
+        } else {
+            $this->expectException("TypeError");
+            $service->expects($this->any())     
+                    ->method("updateUser")
+                    ->will($this->returnValue(new User()));
+
+            $userController->updateAction($service->updateUser($input));
+        }
+        
+    }
+    
+    public function providerUpdateAction() {
+        return [
+            // exception    input     
+            [false,         new User()],
+            [true,          1],
+            [true,          true],
+            [true,          'sdfdg']
+        ];
+    }
+    
+    /**
+     * @dataProvider providerDeleteAction
+     */
+    public function testForDeleteAction($exception, $input) {
+        $service = $this->getMockBuilder("App\Services\UserService")
+                ->setConstructorArgs(['ut'])
+                ->getMock();
+        $userController = new App\Controllers\UserController();
+        $userController->setService($service);
+        
+        if(!$exception){
+            $this->expectException("PDOException");
+            $service->expects($this->once())     
+                    ->method("deleteUser")
+                    ->will($this->returnValue($input));
+
+            $userController->deleteAction($service->deleteUser($input));
+        } else {
+            $this->expectException("PDOException");
+            $service->expects($this->any())     
+                    ->method("deleteUser")
+                    ->will($this->returnValue(false));
+
+            $userController->deleteAction($service->deleteUser($input));
+        }
+    }
+    
+    public function providerDeleteAction() {
+        return [
+            // exception    input      
+            [false,         111],
+            [true,          new User()],
+            [true,          array()],
+            [true,          'sdfdg']
+        ];
+    }
     
     /**
      * @dataProvider providerControllerTest
      */
-    public function testUserController($action, $id) {
+    public function testUserControllerReturningValues($action, $id) {
         $mock = $this->getMockBuilder("App\Controllers\UserController")
-                ->setMethods(["$action"])
+                ->setMethods(["$action", "setService"])
                 ->enableOriginalConstructor()
-                ->setConstructorArgs(['ut'])
                 ->getMock();
         $mockUserClass = $this->getMockBuilder("App\Models\User")
                 ->getMock();
-        //$service = $this->getMockBuilder($className);
         
         if ($id == null) {
             $mock->expects($this->once())
@@ -33,7 +171,7 @@ class UserControllerTest extends TestCase {
     
     public function providerControllerTest() {
         return [
-            // action           id 
+            // action           id    
             ['listAction',      null],
             ['createAction',    null],
             ['updateAction',    10],
@@ -41,4 +179,9 @@ class UserControllerTest extends TestCase {
         ];
     }
     
+}
+
+function getArgument() {
+    $args = func_get_args();
+    return $args;
 }
